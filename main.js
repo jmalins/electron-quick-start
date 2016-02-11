267c49272843aa9773763e8b3a9619a6b1e214da
Jeff Malins <jmalins@iolani.org>
2016-02-11 08:58:54 -1000

    modified for testing

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
+const globalShortcut = electron.globalShortcut;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -11,41 +12,40 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
+  mainWindow = new BrowserWindow({ width: 800, height: 100, frame: false });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
+  });
+  mainWindow.on('focus', function(e) {
+    console.log('focused!');
+    e.sender.webContents.send('focused', 'true');
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
+app.on('ready', function() {
+  createWindow();
+
+  var ret = globalShortcut.register('ctrl+shift+h', function() {
+    console.log('ctrl+shift+h is pressed');
+    if(mainWindow) {
+      mainWindow.focus();
+    }
+  });
+
+  if(!ret) {
+    console.log('registration failed');
   }
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+app.on('window-all-closed', function () {
+  app.quit();
+});
+
+//app.on('activate', function () { });
+app.on('will-quit', function() {
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll();
 });
